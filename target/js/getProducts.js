cc0b3404630976b60b4c40aa0e6ee7dfaf67f927
ayjammoulMTS <jammoul.171@gmail.com>
2023-12-18 19:18:54 +0200

    new updates

--- target/js/getProducts.js
+++ target/js/getProducts.js
@@ -1,88 +1,87 @@
+const preferredOrder = ["Snacks / Chips","Snacks / Chocolate","Snacks / Biscuits","Snacks / Salted Biscuits","Snacks / Wafer","Snacks / Cake","Snacks / Rice Cake","Snacks / Date","Snacks / Musk","Beverage / Cocoa","Beverage / Coffee","Beverage / Tea","Beverage / Syrups","Pantry / Condiments & Dressings","Pantry / Cans","Pantry / Pasta & Grains","Pantry / Oils & Vinegars","Pantry / Processed Cheese","Pantry / Toasts", /* Add other categories here */];
 
 fetch("https://aesthetic-eclair-56d00c.netlify.app/.netlify/functions/api/products-by-category_today")
 .then(response => response.json())
 .then(categories => {
+  // Sort categories based on the preferred order
+  categories.sort((a, b) => {
+    let indexA = preferredOrder.indexOf(a._id);
+    let indexB = preferredOrder.indexOf(b._id);
+    return (indexA === -1 ? Infinity : indexA) - (indexB === -1 ? Infinity : indexB);
+  });
+
   const mainContainer = document.querySelector('#property-grid-item');
 
-  // Loop through each category
   categories.forEach(category => {
-    // Create a new row for the category
     const categoryRow = document.createElement('div');
     categoryRow.classList.add('row');
     categoryRow.innerHTML = `
-    <div class="col-md-8 col-6"><h2>${category._id}</h2></div>
-    <div class="col-md-4 col-6 text-right">
-    <a href="products-category.html?category=${category._id}" class="btn btn-color-b">Show all</a>
-    </div>
-    `;
-    
-    // Loop through the first three products of each category
-    category.products.slice(0, 3).forEach(product => {
-        // Create the product elements
-        const productElem = document.createElement('div');
-        productElem.classList.add('carousel-item-b', 'col-md-4');
-
-        // Add product HTML here
-        productElem.innerHTML = `
-        <div class="card-box-d card-shadow">
-        <div class="img-box-a">
-        <img src="${product.image}" alt="" class="img-a img-fluid" id="product-outside">
+      <div class="col-md-8 col-6"><h2>${category._id}</h2></div>
+      <div class="col-md-4 col-6 text-right">
+        <a href="products-category.html?category=${category._id}" class="btn btn-color-b">Show all</a>
       </div>
-      
+    `;
+
+    category.products.slice(0, 3).forEach(product => {
+      const productElem = document.createElement('div');
+      productElem.classList.add('carousel-item-b', 'col-md-4');
+      productElem.innerHTML = `
+        <div class="card-box-d card-shadow">
+          <div class="img-box-a">
+            <img src="${product.image}" alt="" class="img-a img-fluid" id="product-outside">
+          </div>
           <div class="card-overlay">
             <div class="card-overlay-a-content">
               <div class="card-header-a">
                 <h2 class="card-title-a">
-                  <a href="#">${product.name}
-                    </a>
+                  <a href="product-details.html?id=${product._id}">${product.name}</a>
                 </h2>
               </div>
               <div class="card-body-a">
-               
                 <a href="product-details.html?id=${product._id}" class="link-a">Click here to view
                   <span class="ion-ios-arrow-forward"></span>
                 </a>
               </div>
               <div class="card-footer-a">
-                
+                <!-- Footer content -->
               </div>
             </div>
           </div>
         </div>`;
+      categoryRow.appendChild(productElem);
+    });
 
-        // Add the product element to the category row
-        categoryRow.appendChild(productElem);
-      });
-
-    // Add the category row to the main container
     mainContainer.appendChild(categoryRow);
   });
-  
+
   document.getElementById('preloader').style.display = 'none';
 })
 .catch(err => console.error('An error occurred:', err));
 
 document.addEventListener('DOMContentLoaded', function() {
-  // Function to populate the categories navbar
   function populateCategoriesNavbar() {
-      fetch("https://aesthetic-eclair-56d00c.netlify.app/.netlify/functions/api/products-by-category_today")
-          .then(response => response.json())
-          .then(categories => {
-              const categoriesNavbar = document.getElementById('categories-navbar');
+    fetch("https://aesthetic-eclair-56d00c.netlify.app/.netlify/functions/api/products-by-category_today")
+      .then(response => response.json())
+      .then(categories => {
+        // Sort categories based on the preferred order
+        categories.sort((a, b) => {
+          let indexA = preferredOrder.indexOf(a._id);
+          let indexB = preferredOrder.indexOf(b._id);
+          return (indexA === -1 ? Infinity : indexA) - (indexB === -1 ? Infinity : indexB);
+        });
 
-              categories.forEach(category => {
-                  const categoryListItem = document.createElement('li');
-                  const categoryLink = document.createElement('a');
-                  categoryLink.href = `products-category.html?category=${category._id}`;
-                  categoryLink.innerText = category._id;
-
-                  categoryListItem.appendChild(categoryLink);
-                  categoriesNavbar.appendChild(categoryListItem);
-              });
-          })
-          .catch(err => console.error('An error occurred:', err));
+        const categoriesNavbar = document.getElementById('categories-navbar');
+        categories.forEach(category => {
+          const categoryListItem = document.createElement('li');
+          const categoryLink = document.createElement('a');
+          categoryLink.href = `products-category.html?category=${category._id}`;
+          categoryLink.innerText = category._id;
+          categoryListItem.appendChild(categoryLink);
+          categoriesNavbar.appendChild(categoryListItem);
+        });
+      })
+      .catch(err => console.error('An error occurred:', err));
   }
 
-  // Execute the function to populate categories
   populateCategoriesNavbar();
 });
--- target/js/productDetails.js
+++ target/js/productDetails.js
@@ -1,37 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 const productId = urlParams.get('id');
 
-// fetch(`https://aesthetic-eclair-56d00c.netlify.app/.netlify/functions/api/product_today/${productId}`)
-//   .then(response => response.json())
-//   .then(product => {
-//     // Update the product's name
-//     const productNameElem = document.querySelector('.title-single');
-//     productNameElem.textContent = product.name;
-
-//     const productCatElem = document.querySelector('#cat-o-product');
-//     productCatElem.textContent = product.category;
-
-//     const productImageElem = document.querySelector('#image-details');
-//     productImageElem.src = ''; 
-//     productImageElem.src = product.image  + '?v=' + new Date().getTime();
-//     console.log(product.image);
-//     console.log(productImageElem);
-
-//     // Update the product's description
-//     const productDescriptionElem = document.querySelector('.description.color-text-a');
-//     productDescriptionElem.textContent = product.description;
-
-  
-//     // const productImageElem = document.querySelector('image-details');
-//     // productImageElem.src = product.image;
-
-//     // If you have a place for the product's price, you can update it as well
-//     // const productPriceElem = document.querySelector('selector-for-product-price-element');
-//     // productPriceElem.textContent = product.price;
-//   })
-//   .catch(err => console.error('An error occurred:', err));
-
-
 
 const url = `https://aesthetic-eclair-56d00c.netlify.app/.netlify/functions/api/product_today/${productId}`; // Replace with the correct URL to fetch a single product
 fetch(`https://aesthetic-eclair-56d00c.netlify.app/.netlify/functions/api/product_today/${productId}`)
@@ -85,7 +54,7 @@ function displayProductDetails(product) {
       <div class="row">
       <!-- Image Column -->
       <div class="col-sm-12 col-md-6">
-      <img src="${product.image}" alt="" id="image-details" >
+      <img src="${product.image}" alt="" id="image-details">
           <div id="property-single-carousel" class="owl-carousel owl-arrow gallery-property">
             <div class="carousel-item-b">
               <img src="images/1.png" alt="" id="image-details" >
